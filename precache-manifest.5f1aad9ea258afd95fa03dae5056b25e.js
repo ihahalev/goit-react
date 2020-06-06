be3c37bbe2b01a0746df1c2f05b357bcbe4ec3a6
ihahalev <i_hahalev@ukr.net>
2020-06-06 16:09:28 +0300

    Updates

---
+++ precache-manifest.5f1aad9ea258afd95fa03dae5056b25e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d88f1679f398b0a6e97522477f4a0a",
+    "url": "/goit-react/index.html"
+  },
+  {
+    "revision": "69132ae320e10f6ff14e",
+    "url": "/goit-react/static/css/main.ddc083ff.chunk.css"
+  },
+  {
+    "revision": "059bdb102651889baa30",
+    "url": "/goit-react/static/js/2.14e7d4d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react/static/js/2.14e7d4d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69132ae320e10f6ff14e",
+    "url": "/goit-react/static/js/main.a30912a2.chunk.js"
+  },
+  {
+    "revision": "4b97352113d8bd517893",
+    "url": "/goit-react/static/js/runtime-main.4eb98d7f.js"
+  }
+]);
